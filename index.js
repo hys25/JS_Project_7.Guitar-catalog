28d1e81f560932c6ce4720bedd33a2a5e09d2cad
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2020-08-04 17:30:00 +0300

    AJAX product catalogue request (Fetch API)

---
+++ index.js
@@ -0,0 +1,23 @@
+function render () {
+          const productsStore = localStorageUtil.getProducts();
+
+          headerPage.render(productsStore.length);
+          productsPage.render();
+}
+
+let CATALOG = [];
+
+fetch('server/catalog.json')
+          .then(res => res.json())
+          .then(body => {
+                    CATALOG = body;
+                    render();
+          })
+          .catch(error => {
+                    console.log(error);
+          });
+
+
+
+
+
